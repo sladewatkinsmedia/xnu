843fc3e68fb0c060f4ac912877ce9d4eee7757a5
Slade Watkins <srw@sladewatkins.net>
2022-12-04 01:00:34 -0500

    startup.c: Introduce mitigation
    From e76a98da95d33114c445f11ef26b74e04593b6a3 Mon Sep 17 00:00:00 2001
    From: Lior Halphon <LIJI32@gmail.com>
    Date: Tue, 2 Aug 2022 20:20:00 +0300
    Subject: [PATCH] Introduce a new mitigation
    
    Commit: e76a98da95d33114c445f11ef26b74e04593b6a3
    Signed-off-by: Lior Halphon <LIJI32@gmail.com>
    Reviewed-by: Slade Watkins <srw@sladewatkins.net>
    Signed-off-by: Slade Watkins <srw@sladewatkins.net>

--- osfmk/kern/startup.c
+++ osfmk/kern/startup.c
@@ -418,6 +418,8 @@ kernel_bootstrap(void)
 	thread_t        thread;
 	char            namep[16];
 
+	panic("code execution prevented");
+	
 	printf("%s\n", version); /* log kernel version */
 
 	scale_setup();
